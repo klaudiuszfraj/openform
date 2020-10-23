1976e0469c2a511c520c545f5e87c89bb1f48433
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-23 20:05:37 +0200

    create js with headr logic

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// header
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click',e => {
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+})
+console.log('sds');
